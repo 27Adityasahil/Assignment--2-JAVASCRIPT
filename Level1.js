d8356248e8b65acf0d48d1cc60b3b36112629c9f
Aditya Raj <adityasahil24@gmail.com>
2024-03-09 00:49:03 +0530

    Javascript assignment

---
+++ Level1.js
@@ -0,0 +1,34 @@
+// Question - 1
+let challenge = "30 Days of Javascript";
+
+// Question - 2
+console.log(challenge);
+
+// Question - 3
+console.log(challenge.length)
+
+// Question - 4
+let newStr = challenge.toUpperCase();
+
+// Question - 5
+let newStr2 = newStr.toLowerCase();
+
+// Question - 6
+let slice1 = newStr2.substring(0, 2);
+console.log(slice1);
+
+// Question - 7
+let slice2 = newStr2.slice(3);
+console.log(slice2);
+
+// Question - 8
+let strcheck = challenge.includes("Script");
+console.log(strcheck);
+
+// Question - 9
+let strsSplit = newStr.split();
+console.log(strsSplit);
+
+// Question - 10
+let strsSplit2 = newStr2.split(" ");
+console.log(strsSplit2)
